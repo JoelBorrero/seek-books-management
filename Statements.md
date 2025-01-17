ec3d0506276bb56f8984d2047beba8b8b81f186e
Joel Borrero <joeborrero13@gmail.com>
2025-01-17 20:45:09 +0100

    ✅ Include Swagger UI

--- Statements.md
+++ Statements.md
@@ -57,7 +57,7 @@ El reto debe ser entregado máximo **48 horas** luego de haber recibido el reto 
 
 - [ ] Repositorio de **GitHub** con el código fuente.
 - [ ] URL de la API desplegada en **Amazon Web Services (AWS)** (o en **Azure** o **GCP**) usando los servicios en su capa free.
-- [ ] Documentación de los **endpoints** en **Swagger**.
+- [x] Documentación de los **endpoints** en **Swagger**.
 - [ ] Una colección en **Postman** con las llamadas a cada una de las APIs, incluyendo un par de casos ya grabados:
   - [ ] Uno con **HTTP Status 200** (exitoso).
   - [ ] Uno con **HTTP Status 500** (para caso de error) por cada endpoint.
--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,24 @@
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Books Api V1",
+        default_version='v1',
+        description="Project built by Joel Borrero",
+        contact=openapi.Contact(email="joeborrero13@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[
+        permissions.AllowAny,
+    ],
+)
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include('apps.book.urls')),
 ]
